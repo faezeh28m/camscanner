36ba15424b39d0932ffbb718696832922cdb6046
faezehmovahedi <47081770+faezeh28m@users.noreply.github.com>
2022-02-17 15:42:05 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-from email.mime import image
-from itertools import count
-import this
-from tracemalloc import start
-from typing import Counter
+
 import cv2
-from cv2 import imshow
-from cv2 import destroyAllWindows
-from cv2 import imread
-from cv2 import THRESH_BINARY
-from cv2 import THRESH_BINARY_INV
-from cv2 import threshold
-from cv2 import Canny
 import numpy as np
 from pip import main
 
 def take_pic():
-    # while True:
+
+    while True:
 
         choice = input ('1) camera\n2) URL\nYour choice: ')
         if choice == '1':
@@ -26,21 +16,26 @@ def take_pic():
             cv2.imshow("GeeksForGeeks", image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
-            # break
+
+            return image
 
         elif choice == '2':
-            url = input('enter URL (exampale: C:\\Users\\faezeh\\Desktop\\imagename.jpg) : ')
-            image = cv2.imread( url )
-            # print(image)
+
+            """If you want to enter your address, leave the following two lines uncomment"""
+            # url = input("print your url: ")
+            # image = cv2.imread( url )
+            """If you want to use the default address run next line"""
+            image = cv2.imread( 'image.png' )
+            
             cv2.imshow("image", image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
-            # break
 
-        # else:
-        #     print('Your choice is incorrect')
+            return image
 
-        return image
+        else:
+            print('Your choice is incorrect')
+
 
 def cordinate(image):
     y = image.shape[0]
@@ -54,74 +49,90 @@ def cordinate(image):
 
 def starting_point(image):
     img2 = image.copy()
-    # img2 = 255 - image
     gray_img = cv2.cvtColor(img2 , cv2.COLOR_BGR2GRAY)
-    # blur = cv2.blur(gray_img,(1,2))
+    blur = cv2.blur(gray_img,(2,2))
    
-    edges = cv2.Canny(gray_img,60,200)
-    th3 = cv2.adaptiveThreshold(edges,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 5 , 5)
+    edges = cv2.Canny(blur,60,200)
+    kernel = np.ones((4,4),np.uint8)
+    dialted = cv2.dilate(edges , kernel , iterations= 2)
+
+
+
+    # th3 = cv2.adaptiveThreshold(gray_img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 5 , 5)
+    # th3 = 255 - th3
+    # inv_edge = 255 - edges
+    # erode = cv2.erode(edges,kernel,iterations = 1)
+
+
     # ret , th3 = cv2.threshold(edges, 0 , 255 , cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    # kernel = np.ones((4,4),np.uint8)
-    # dilation = cv2.erode(th3,kernel,iterations = 1)
     # _ , gray_img2 = cv2.threshold(th3 , 160 , 255 , cv2.THRESH_BINARY)
 
-    contours , _ = cv2.findContours(th3 , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_NONE)
-    # print(len(contours))
-    
-    my_contour = contours[0]
-    count = len(contours)
-    #choice biggest contour as my_countour for perspective
-    for i in range(count - 1):
-        if(cv2.contourArea(contours[i])) < (cv2.contourArea(contours [i+1])):
-            my_contour = contours[i+1]
+    contours , _ = cv2.findContours(dialted , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_NONE)
 
+    max_area = 0
+    max_contour= []
+
+    for cont in contours:
+        area = cv2.contourArea(cont)
+        
+        if area > max_area :
+            max_area = area
+            max_contour = cont
     
-    area = cv2.approxPolyDP( my_contour, len(my_contour)*0.1, True) 
-    
-    hull = cv2.convexHull(area, _ , True, True)
-    # cv2.drawContours(image, [my_contour], -1, (255,0,0), 3)
-     
+    epsilon = 0.1*cv2.arcLength(max_contour,True)
+    aprox_contours = cv2.approxPolyDP( max_contour, epsilon , True)
+    hull = cv2.convexHull(aprox_contours, _ , True, True)
+
+    cv2.drawContours(image,[hull] , -1, (255,0,0), 3)
     cv2.imshow("image : ", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    return hull
+    return hull.reshape((4,2))   # Changing the shape of the output convex hull
 
 def distance(start_point , end_point):
     return ((((start_point[0] - end_point[0]) ** 2) + ((start_point[1] - end_point[1]) ** 2)) ** (1/2))
 
 def transfer(starting_point , end_point , image): 
-    arranged_starting_point = []  
-    for i in range(4):
-        end = end_point[i]
-        for j in range(3):
-            start = starting_point[0]
-            print('start: ' , start , 'end = ',end)
-            if(distance(starting_point[j],end_point[i])) < (distance(starting_point[j+1],end_point[i])):
-                start = starting_point[j]
+    
+    arranged_end_point = []  
+    
+    for org_point in starting_point:
+        min_distance = 1000000000
+        min_point = []
 
-            arranged_starting_point.append(start)
+        for dest_point in end_point:
+            cur_distance = distance(dest_point , org_point)
 
-    rows,cols,_=image.shape
-    M=cv2.getPerspectiveTransform(starting_point,end_point)
-    dst=cv2.warpPerspective(image,M,(cols,rows))
+            if cur_distance < min_distance:
+                min_point = dest_point
+                min_distance = cur_distance
 
-    return dst
+        arranged_end_point.append(min_point)
+
+    arranged_end_point = np.array(arranged_end_point)
+
+    src = np.expand_dims(starting_point , axis= 0).astype(np.float32)
+    dest = np.expand_dims(arranged_end_point , axis= 0).astype(np.float32)
+
+    rows, cols, _ = image.shape
+    M = cv2.getPerspectiveTransform( src, dest)
+    # print(M)
+    final_img = cv2.warpPerspective(image,M,(cols,rows))
+
+    return final_img
 
 
 def main(): 
     image = take_pic()
     # imread('image.png')
-    scanned_img = transfer(starting_point(image) , cordinate(image) , image)
+    st_point = starting_point(image)
+    # print(st_point)
+    cord =  cordinate(image)
+    scanned_img = transfer(st_point , cord , image)
 
     cv2.imshow("image", scanned_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    starting_point(image)
-
-# take_pic()
-
-
-# cordinate(image)
 
 main()
